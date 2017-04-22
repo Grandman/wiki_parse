46e23ffd6158dfd56d1faaed3017471d425826dc
Grandman <root@grandman73.ru>
2017-04-22 23:54:10 +0400

    temp version with owl example

--- parse.py
+++ parse.py
@@ -2,9 +2,10 @@ import sys
 import wikipedia
 import treelib
 from treelib import Node, Tree
+from owlready import *
 
 query = sys.argv[1]
-second_query = sys.argv[2]
+second_query = sys.argv[2].lower()
 
 print("query:" + query)
 print("second query:" + second_query)
@@ -12,6 +13,15 @@ print("second query:" + second_query)
 lang = 'ru'
 
 print("lang: " + lang)
+
+# loading ontology
+
+programming = get_ontology('file:///code/programming.owl')
+programming.load()
+classes = programming.classes
+string_classes = [str(e) for e in classes]
+
+print(programming.classes)
 
 wikipedia.set_lang(lang)
 page = wikipedia.page(query)
@@ -27,11 +37,17 @@ while found == False:
     print("Total at level " + str(depth) + ": " + str(len(nodes)))
     for node in nodes:
       link = node.tag
+      if not any(link.replace(" ", "_").lower() == e for e in string_classes):
+        continue
+      # print(link.lower())
+      # print(string_classes)
       try:
         page = wikipedia.page(link)
+        # print(link.lower())
         for link in page.links:
-          tree.create_node(link, parent = node.identifier)
-        if second_query in page.links:
+          # print(link)
+          tree.create_node(link.lower(), parent = node.identifier)
+        if second_query in [s.lower() for s in page.links]:
             found = True
             print('FOUNDED')
             print('FOUNDED')
